e1af3d6a024653525765b6b8ef5ede5c5dcdc5b5
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-01-29 15:49:31 +0100

    Added userrepository/service/serviceimpl
    ==> CRUD

--- src/main/java/com/ehb/connected/domain/impl/users/repositories/UserRepository.java
+++ src/main/java/com/ehb/connected/domain/impl/users/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.ehb.connected.domain.impl.users.repositories;
 
 import com.ehb.connected.domain.impl.users.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
