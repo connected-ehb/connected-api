7032f4fe81fe7dbba2120da95c5761e7ced21041
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-01-29 10:58:01 +0100

    deleted imports that are not used

--- src/test/java/com/ehb/connected/domain/impl/projects/service/ProjectServiceImplTest.java
+++ src/test/java/com/ehb/connected/domain/impl/projects/service/ProjectServiceImplTest.java
@@ -1,14 +1,11 @@
 package com.ehb.connected.domain.impl.projects.service;
 
-import com.ehb.connected.domain.impl.assignments.entities.Assignment;
 import com.ehb.connected.domain.impl.projects.entities.Project;
 import com.ehb.connected.domain.impl.projects.repositories.ProjectRepository;
-import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
