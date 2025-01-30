14d37272d3f12bd662815cf8f3ae744f0f8e7fd4
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-01-30 13:02:41 +0100

    CON-6 Added id to function, and made sure updating a project can only be done before it has been approved

--- src/main/java/com/ehb/connected/domain/impl/projects/service/ProjectService.java
+++ src/main/java/com/ehb/connected/domain/impl/projects/service/ProjectService.java
@@ -8,7 +8,7 @@ public interface ProjectService {
     List<Project> getAllProjects();
     Project getProjectById(Long id);
     Project createProject(Project project);
-    Project updateProject(Project project);
+    Project updateProject(Long id, Project project);
     void deleteProject(Long id);
 
     void approveProject(Long id);
