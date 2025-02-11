00ee9258d93de5715d5bbaa9254891a67f9e34e4
Gill Mertens <gill.mertens@student.ehb.be>
2025-02-11 10:42:06 +0100

    chore: add the owner of a course

--- src/main/java/com/ehb/connected/domain/impl/courses/entities/Course.java
+++ src/main/java/com/ehb/connected/domain/impl/courses/entities/Course.java
@@ -1,9 +1,15 @@
 package com.ehb.connected.domain.impl.courses.entities;
 
+import com.ehb.connected.domain.impl.users.entities.User;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,12 +27,21 @@ import java.util.List;
 @AllArgsConstructor
 public class Course {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String uuid;
+
     private String name;
+
     private LocalDateTime start_at;
+
     private LocalDateTime end_at;
 
     @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
     private List<Assignment> assignments = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
 }
