cad41b308a61569bc12d00a3ec189f93a1585b13
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-02-11 16:11:06 +0100

    CON-145 - can find all assignments by courseId

--- src/main/java/com/ehb/connected/domain/impl/assignments/repositories/AssignmentRepository.java
+++ src/main/java/com/ehb/connected/domain/impl/assignments/repositories/AssignmentRepository.java
@@ -4,6 +4,9 @@ import com.ehb.connected.domain.impl.assignments.entities.Assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
+    List<Assignment> findByCourseId(Long courseId);
 }
