baf40df3d38e4756ffc3f6fa65256eb12646a34e
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-02-12 12:46:14 +0100

    CON-100 - can find all deadlines by assignment and also by restriction

--- src/main/java/com/ehb/connected/domain/impl/deadlines/repositories/DeadlineRepository.java
+++ src/main/java/com/ehb/connected/domain/impl/deadlines/repositories/DeadlineRepository.java
@@ -1,9 +1,16 @@
 package com.ehb.connected.domain.impl.deadlines.repositories;
 
 import com.ehb.connected.domain.impl.deadlines.entities.Deadline;
+import com.ehb.connected.domain.impl.deadlines.enums.DeadlineRestriction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DeadlineRepository extends JpaRepository<Deadline, Long> {
+    List<Deadline> findAllByAssignmentId(Long assignmentId);
+
+    List<Deadline> findAllByAssignmentIdAndRestriction(Long assignmentId, DeadlineRestriction restriction);
+
 }
