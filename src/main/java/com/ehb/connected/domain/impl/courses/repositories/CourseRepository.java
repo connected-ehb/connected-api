f3aafdefd352de17eae6043087fcefe4aabcd90c
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-02-12 16:59:10 +0100

    CON-147 - find assignment/course by canvasid

--- src/main/java/com/ehb/connected/domain/impl/courses/repositories/CourseRepository.java
+++ src/main/java/com/ehb/connected/domain/impl/courses/repositories/CourseRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
     List<Course> findByOwner(User owner);
     List<Course> findByEnrollmentsCanvasUserId(Long canvasUserId);
+
+    Optional<Course> findByCanvasCourseId(Long canvasCourseId);
 }
