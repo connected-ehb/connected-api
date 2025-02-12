4324d44aa04f4682aade34b525c145be0c72ee00
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-02-12 14:15:50 +0100

    CON-155 - returns projectId instead of whole project entity

--- src/main/java/com/ehb/connected/domain/impl/applications/dto/ApplicationDto.java
+++ src/main/java/com/ehb/connected/domain/impl/applications/dto/ApplicationDto.java
@@ -1,9 +1,7 @@
 package com.ehb.connected.domain.impl.applications.dto;
 
 import com.ehb.connected.domain.impl.applications.entities.ApplicationStatusEnum;
-import com.ehb.connected.domain.impl.projects.entities.Project;
 import com.ehb.connected.domain.impl.users.dto.UserDetailsDto;
-import com.ehb.connected.domain.impl.users.entities.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +13,6 @@ public class ApplicationDto {
     private Long id;
     private String motivationMd;
     private ApplicationStatusEnum status;
-    private Project project;
+    private Long projectId;
     private UserDetailsDto applicant;
 }
