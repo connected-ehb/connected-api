1aec2a54cdd68968e6f30ab72a255adb752ab7ea
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-02-12 14:16:17 +0100

    CON-155 - now uses projectid and principal to get required data

--- src/main/java/com/ehb/connected/domain/impl/applications/service/ApplicationService.java
+++ src/main/java/com/ehb/connected/domain/impl/applications/service/ApplicationService.java
@@ -1,12 +1,15 @@
 package com.ehb.connected.domain.impl.applications.service;
 
+import com.ehb.connected.domain.impl.applications.dto.ApplicationCreateDto;
+import com.ehb.connected.domain.impl.applications.dto.ApplicationDto;
 import com.ehb.connected.domain.impl.applications.entities.Application;
 
+import java.security.Principal;
 import java.util.List;
 
 public interface ApplicationService {
     Application getApplicationById(Long id);
-    Application createApplication(Application application);
+    ApplicationDto createApplication(Principal principal, Long projectId, ApplicationCreateDto application);
     Application updateApplication(Application application);
     void deleteApplication(Long id);
     List<Application> findAllApplications(Long id);
