a7632380687f307f801a5ad6db0b8344f1c27382
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-02-17 18:05:22 +0100

    feat: prevent project creation for users with pending, approved, or published projects

--- src/main/java/com/ehb/connected/domain/impl/projects/repositories/ProjectRepository.java
+++ src/main/java/com/ehb/connected/domain/impl/projects/repositories/ProjectRepository.java
@@ -2,6 +2,7 @@ package com.ehb.connected.domain.impl.projects.repositories;
 
 import com.ehb.connected.domain.impl.projects.entities.Project;
 import com.ehb.connected.domain.impl.projects.entities.ProjectStatusEnum;
+import com.ehb.connected.domain.impl.users.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,6 @@ import java.util.List;
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     List<Project> findAllByAssignmentId(Long assignmentId);
     List<Project> findAllByAssignmentIdAndStatus(Long AssignmentId, ProjectStatusEnum status);
+
+    boolean existsByMembersContainingAndStatusIn(User user, List<ProjectStatusEnum> pending);
 }
