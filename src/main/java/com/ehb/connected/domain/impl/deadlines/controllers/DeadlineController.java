0b286bc045e1c07cc752325c6524dab77f6d11b1
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-02-19 18:17:32 +0100

    feat: update endpoint path for DeadlineController to include API prefix

--- src/main/java/com/ehb/connected/domain/impl/deadlines/controllers/DeadlineController.java
+++ src/main/java/com/ehb/connected/domain/impl/deadlines/controllers/DeadlineController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/deadlines")
+@RequestMapping("/api/deadlines")
 @RequiredArgsConstructor
 public class DeadlineController {
 
