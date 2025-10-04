a5be1eb5fe2c26af7e04ee01856f83dde55c1888
BRYAN <bryan1999langhendries@hotmail.com>
2025-10-04 11:54:27 +0200

    bugfix: hasmembers was incorrect

--- src/main/java/com/ehb/connected/domain/impl/enrollments/services/EnrollmentService.java
+++ src/main/java/com/ehb/connected/domain/impl/enrollments/services/EnrollmentService.java
@@ -2,6 +2,10 @@ package com.ehb.connected.domain.impl.enrollments.services;
 
 import com.ehb.connected.domain.impl.courses.entities.Course;
 
+import java.util.List;
+
 public interface EnrollmentService {
     void enrollUser(Course course, Long canvasUserId);
+    void replaceCourseEnrollments(Course course, List<Long> canvasUserIds);
+    long countByCourseId(Long courseId);
 }
--- src/main/java/com/ehb/connected/domain/impl/enrollments/services/EnrollmentServiceImpl.java
+++ src/main/java/com/ehb/connected/domain/impl/enrollments/services/EnrollmentServiceImpl.java
@@ -3,8 +3,13 @@ package com.ehb.connected.domain.impl.enrollments.services;
 import com.ehb.connected.domain.impl.courses.entities.Course;
 import com.ehb.connected.domain.impl.enrollments.entities.Enrollment;
 import com.ehb.connected.domain.impl.enrollments.repositories.EnrollmentRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +22,21 @@ public class EnrollmentServiceImpl implements EnrollmentService {
         enrollment.setCanvasUserId(canvasUserId);
         enrollment.setCourse(course);
         enrollmentRepository.save(enrollment);
+    }
+
+    @Transactional
+    @Override
+    public void replaceCourseEnrollments(Course course, List<Long> canvasUserIds) {
+        enrollmentRepository.deleteByCourseId(course.getId());
+
+        Set<Long> unique = new HashSet<>(canvasUserIds);
+        for (Long id : unique) {
+            enrollUser(course, id);
+        }
+    }
+
+    @Override
+    public long countByCourseId(Long courseId) {
+        return enrollmentRepository.countByCourseId(courseId);
     }
 }
